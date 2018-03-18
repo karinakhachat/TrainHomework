8fc9a8e5ac1752c6d7313d568e2623b52b29d917
Karina Khachatryan <karinakhachat@Karinas-Air.fios-router.home>
2018-03-17 21:33:15 -0400

    first commit

---
+++ Homework7/firebase.js
@@ -0,0 +1,37 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB-IfrW9jtMzS26BtNEsELys2Ty68Le9pI",
+    authDomain: "choochootrain-3364a.firebaseapp.com",
+    databaseURL: "https://choochootrain-3364a.firebaseio.com",
+    projectId: "choochootrain-3364a",
+    storageBucket: "",
+    messagingSenderId: "86243128591"
+  };
+  
+
+  console.log(config);
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  var trainName= $("#name-input").val();
+  var destination=$("#role-input").val();
+  var firstTrain=moment($("#start-input").val());
+  var frequencyMin=moment($("#rate-input").val());
+
+
+  var NewTrain= {
+      name: trainName,
+      arrival: destination,
+      firstT: firstTrain,
+      frequency: frequencyMin
+  };
+
+  database.ref().push(NewTrain);
+
+  console.log(NewTrain.name);
+  console.log(NewTrain.arrival);
+  console.log(NewTrain.firstT);
+  console.log(NewTrain.frequency);
